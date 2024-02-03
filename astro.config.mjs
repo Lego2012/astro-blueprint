4354ce471a8a6c696204b31269bb046e861afbdb
Leo Merkel <leo@leomerkel.de>
2024-02-03 13:22:12 +0100

    bookmarks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
+// IMPORTS
 import { defineConfig } from 'astro/config';
 import metaTags from "astro-meta-tags";
 import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.example.com',
-  integrations: [metaTags(), icon(), sitemap(), tailwind()]
+   site: 'https://www.example.com',
+   integrations: [metaTags(), icon(), sitemap(), tailwind()]
 });
